c897f6938dc4715403b4b2798c2b8525628ea5be
Remi Tobias <remitobias1@outlook.com>
2020-11-05 18:59:23 +0000

    Add project files.

---
+++ MyTimer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace StudyTimer
+{
+    class MyTimer
+    {
+        private static System.Timers.Timer timer;
+        private static System.Media.SoundPlayer player;
+        private static int seconds;
+        private static int minutes;
+        private static int hours;
+
+        public MyTimer()
+        {
+            seconds = 50;
+            minutes = 24;
+            hours = 0;
+            timer = new System.Timers.Timer(1000);
+        }
+        public void SetTimer()
+        {
+            timer.Enabled = true;
+            timer.Elapsed += OnTimedEvent;
+        }
+
+        public void KillTimer()
+        {
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        public static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            seconds++;
+            if (seconds > 59)
+            {
+                seconds = 0;
+                minutes += 1;
+            }
+
+            // take a break
+            if ((minutes == 25 || minutes == 50) && seconds == 0)
+            {
+                PlaySound();
+                Thread.Sleep(2000);
+                
+            }
+
+            // back to work
+            if (minutes == 30) { PlaySound(); }
+
+            if (minutes > 59) { PlaySound(); minutes = 0; hours += 1; }
+
+            Console.Clear();
+            Console.WriteLine("Press enter to stop the application");
+            Console.WriteLine("Time elapsed: {0}:{1}:{2}", hours, minutes, seconds);
+        }
+
+        private static void PlaySound()
+        {
+            player = new System.Media.SoundPlayer(@"C:\Sounds\buzzer.wav");
+            player.Play();
+            Console.WriteLine("playing sound");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+//using System.Timers;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace StudyTimer
+{
+    class Program : Form
+    {
+        
+
+        //public Button button1;
+
+        /*
+        public Program() 
+        {
+            button1 = new Button();
+            button1.Size = new Size(40, 40);
+            button1.Location = new Point(30, 30);
+            button1.Text = "Click me";
+            this.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+        */
+        static void Main(string[] args)
+        {
+            //Application.EnableVisualStyles();
+            //Application.Run(new Program());
+            MyTimer timer = new MyTimer();
+
+
+            timer.SetTimer();
+            Console.Read();
+            timer.KillTimer();
+            
+        }
+
+        
+    }
+}
